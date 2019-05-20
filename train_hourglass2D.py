44ac26f993629d1625522413c36fcd7952d5067d
ubuntu <mamrani@ethz.ch>
2019-05-20 23:35:24 +0200

    renaming files

---
+++ train_hourglass2D.py
@@ -0,0 +1,116 @@
+"""Copyright (c) 2019 AIT Lab, ETH Zurich, Xu Chen
+
+Students and holders of copies of this code, accompanying datasets,
+and documentation, are not allowed to copy, distribute or modify
+any of the mentioned materials beyond the scope and duration of the
+Machine Perception course projects.
+
+That is, no partial/full copy nor modification of this code and
+accompanying data should be made publicly or privately available to
+current/future students or other parties.
+"""
+
+import tensorflow as tf
+import numpy as np
+import os, sys
+from tqdm import trange
+import utils
+from data import create_dataloader_train
+import resnet_model
+from hourglass2D_model import StackedHourglass
+import time
+from PIL import Image
+
+NUM_SAMPLES= 312188
+
+# Train parameters
+NUM_EPOCHS = 1
+BATCH_SIZE = 4
+LEARNING_RATE = 0.001
+LOG_ITER_FREQ = 50
+SAVE_ITER_FREQ = 2000
+SAMPLE_ITER_FREQ = 100
+
+# Model parameters
+NB_STACKS=4
+SIGMA=1
+
+# Data parameters
+SHUFFLE=True
+DATA_TO_LOAD="pose2d"
+BATCHES_TO_PREFETCH=300
+
+# Paths
+CURR_DIR = "."
+LOG_PATH = os.path.join(CURR_DIR, "log", utils.timestamp())
+CHECKPOINTS_PATH = os.path.join(LOG_PATH, "checkpoints")
+CLUSTER_PATH = "/cluster/project/infk/hilliges/lectures/mp19/project2/"
+if os.path.exists(CLUSTER_PATH):
+    DATA_PATH = CLUSTER_PATH
+else:
+    DATA_PATH = CURR_DIR
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.visible_device_list = "0"
+with tf.Session(config=config) as sess:
+    
+    # load dataset of batched pairs (image, pose), means and stddev
+    dataset, p2d_mean, p2d_std = create_dataloader_train(data_root=DATA_PATH, batch_size=BATCH_SIZE, batches_to_prefetch=BATCHES_TO_PREFETCH, data_to_load=DATA_TO_LOAD, shuffle=SHUFFLE)
+    im, p2d_gt = dataset # split the pairs (i,e unzip the tuple). When running one, the other also moves to the next elem (i,e same iterator)
+
+    # define model
+    model = StackedHourglass(nb_stacks=NB_STACKS, sigma=SIGMA)
+    
+    # build the model
+    all_heatmaps_pred, p2d_pred = model(im, training=True)
+#    sys.exit(0)
+    
+    # compute loss
+    loss = model.compute_loss(p2d_gt, all_heatmaps_pred)
+#    sys.exit(0)
+
+    # define trainer
+    train_op, global_step = model.get_train_op(loss, learning_rate=LEARNING_RATE)
+#    sys.exit(0)
+
+    # visualization related
+    tf.summary.scalar("loss", loss)
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter(CHECKPOINTS_PATH, sess.graph)
+
+    # initialize
+    tf.global_variables_initializer().run()
+
+    # define model saver
+    saver = tf.train.Saver(tf.global_variables())
+
+    # training loop
+    with trange(int(NUM_EPOCHS * NUM_SAMPLES / BATCH_SIZE)) as t:
+        for i in t:
+
+	        # display training status
+            epoch_cur = i * BATCH_SIZE/ NUM_SAMPLES # nb of epochs completed (e,g 1.5 => one epoch and a half)
+            iter_cur = (i * BATCH_SIZE ) % NUM_SAMPLES # nb of images processed in current epoch
+            t.set_postfix(epoch=epoch_cur,iter_percent="%d %%"%(iter_cur/float(NUM_SAMPLES)*100) ) # update displayed info, iter_percent = percentage of completion of current iteration (i,e epoch)
+
+            # vis
+            if (i+1) % SAMPLE_ITER_FREQ == 0:
+                _, images, p2d_gt_arr, p2d_pred_arr = sess.run([train_op, im, p2d_gt, p2d_pred])
+
+                image = ((images[0]+1)*128.0).transpose(1,2,0).astype("uint8") # unnormalize, put in channels_last format and cast to uint8
+                img = np.asarray(Image.fromarray(image, "RGB")) # necessary conversion for cv2
+                save_dir = os.path.join(LOG_PATH, "train_samples")
+                utils.save_p2d_image(img, p2d_gt_arr[0], p2d_pred_arr[0], save_dir, i+1)
+            
+            elif (i+1) % LOG_ITER_FREQ == 0:
+                _, summary = sess.run([train_op, merged])
+                train_writer.add_summary(summary, i+1)
+            else:
+                _, = sess.run([train_op])
+
+            # save model
+            if (i+1) % SAVE_ITER_FREQ == 0:
+                saver.save(sess,os.path.join(CHECKPOINTS_PATH,"model"),global_step=i+1)
+
+    saver.save(sess,os.path.join(CHECKPOINTS_PATH,"model"),global_step=int(NUM_EPOCHS * NUM_SAMPLES / BATCH_SIZE)) # save at the end of training
--- vis.py
+++ vis.py
@@ -26,7 +26,7 @@ else:
     
 max_num = 10 # max number of images to show with their 2D joints + 3D skeleton
 
-def show3Dpose(channels, ax): # blue, orange
+def show3Dpose(channels, ax, nb_joints_to_show=17):
 
     vals = np.reshape( channels, (17, -1) )
     I = np.array([0, 1, 2, 0, 4, 5, 0, 7, 8, 9, 8, 11, 12, 8, 14, 15])  # start points
@@ -34,21 +34,23 @@ def show3Dpose(channels, ax): # blue, orange
     LR = np.array([1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,0,0,0], dtype=bool)
 
     # Make connection matrix
-    for i in np.arange( len(I) ):
-        x, y, z = [np.array( [vals[I[i], j], vals[J[i], j]] ) for j in range(3)]
-        ax.plot(x, y, z, lw=2, c='r' if LR[i] else 'b')
+    for i in np.arange( nb_joints_to_show-1):
+        x, y, z = [np.array( [vals[I[i], j], vals[J[i], j]] ) for j in range(3)] # each of x, y and z is a pair (x_start, x_end), (y_start, y_end), (z_start, z_end) with coordinates 
+                                                                                 # of start and end joints 
+        ax.plot(x, y, z, lw=2, c='r' if LR[i] else 'b') # plot the line connecting the start and end point using x, y and z with line width = 2 and color "r"="red" or "b"="blue"
 
     RADIUS = 750 # space around the subject
-    xroot, yroot, zroot = vals[0,0], vals[0,1], vals[0,2]
+    xroot, yroot, zroot = vals[0,0], vals[0,1], vals[0,2] # the root's coordinates
     ax.set_xlim3d([-RADIUS+xroot, RADIUS+xroot])
     ax.set_zlim3d([-RADIUS+zroot, RADIUS+zroot])
     ax.set_ylim3d([-RADIUS+yroot, RADIUS+yroot])
 
     ax.view_init(elev=-90., azim=-90)
-
-    ax.get_xaxis().set_ticklabels([])
-    ax.get_yaxis().set_ticklabels([])
-    ax.set_zticklabels([])
+    
+    # remove axis labels and set aspect ratio to be equal across axes
+#    ax.set_xticklabels([])
+#    ax.set_yticklabels([])
+#    ax.set_zticklabels([])
     ax.set_aspect('equal')
 
 
@@ -58,15 +60,28 @@ all_image_paths = [os.path.join(DATA_PATH, "images", path[:-1]) for path in all_
 
 annotations_path = os.path.join(DATA_PATH,"annot","train.h5")
 annotations = h5py.File(annotations_path, 'r')
+np.set_printoptions(suppress=True)
 
 for i,path in enumerate(all_image_paths):
     if i >= max_num: # only show the max_num images with their 2D joints + 3D skeleton
         break;
-        
+# Some interesting images for 3d pose:
+#   image i = 223431 has max distance between any pair of joints along x-axis
+#   image i = 141152 has max distance between any pair of joints along y-axis
+#   image i = 152072 has max distance between any pair of joints along z-axis
+#   image i = 98452 has max x-value for some joint in it
+#    if i != 152072:
+#        continue
+    print(path)
+
+# empirically, every pixel in 2d correspond to around 6.5 mm distance in 3d along x axis
+# so given a joint j with x_j,2d in pixels and x_j,3d in mm, we can roughly convert as follows: x_j,3d = (x_j,2d - x_root,2d) * 6.5 (with some of around 7 pixels, i,e around 7*6.5mm since 6.5 is 
+# just an estimate)
+
     fig = plt.figure()
     ax_img = fig.add_subplot(131)
     ax_p2d = fig.add_subplot(132)
-    ax_p3d = fig.add_subplot(133, projection='3d')
+    ax_p3d = fig.add_subplot(133, projection='3d') # the 3d axes, x-axis grows to the right, y-axis grows to the bottom, z-axis grows towards the screen, origin is at the pelvis (root joint)
 
     image = cv2.imread(path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -80,5 +95,8 @@ for i,path in enumerate(all_image_paths):
     ax_p2d.imshow(image_p2d)
 
     p3d = annotations["pose3d"][i]
+    print(annotations["pose2d"][i])
+    print(p3d)
+    print(p3d.shape)
     show3Dpose(p3d, ax_p3d)
     plt.show()
