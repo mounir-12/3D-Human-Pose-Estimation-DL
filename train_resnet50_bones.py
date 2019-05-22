d21735739c00aea8253adbced5102525ec878c37
ubuntu <mamrani@ethz.ch>
2019-05-22 15:04:25 +0200

    correcting some mistakes, others left

--- train_resnet50_bones.py
+++ train_resnet50_bones.py
@@ -17,7 +17,7 @@ from tqdm import trange
 import utils
 from data import create_dataloader_train
 import resnet_model
-from hourglass2D_model import StackedHourglass
+from resnet50_bones_model import Resnet_50
 import time
 from PIL import Image
 
@@ -50,7 +50,7 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 config.gpu_options.visible_device_list = "0"
 with tf.Session(config=config) as sess:
-	dataset, p3d_mean, p3d_std = create_dataloader_train(data_root=DATA_PATH, batch_size=BATCH_SIZE, batches_to_prefetch=BATCHES_TO_PREFETCH, data_to_load=DATA_TO_LOAD, shuffle=SHUFFLE)
+    dataset, p3d_mean, p3d_std = create_dataloader_train(data_root=DATA_PATH, batch_size=BATCH_SIZE, batches_to_prefetch=BATCHES_TO_PREFETCH, data_to_load=DATA_TO_LOAD, shuffle=SHUFFLE)
     im, pose_gt = dataset # split the pairs (i,e unzip the tuple). When running one, the other also moves to the next elem (i,e same iterator)
 
     model = Resnet_50(nb_joints = 17)
@@ -69,7 +69,7 @@ with tf.Session(config=config) as sess:
     with trange(int(NUM_EPOCHS * NUM_SAMPLES / BATCH_SIZE)) as t:
         for i in t:
 
-	        # display training status
+            # display training status
             epoch_cur = i * BATCH_SIZE/ NUM_SAMPLES # nb of epochs completed (e,g 1.5 => one epoch and a half)
             iter_cur = (i * BATCH_SIZE ) % NUM_SAMPLES # nb of images processed in current epoch
             t.set_postfix(epoch=epoch_cur,iter_percent="%d %%"%(iter_cur/float(NUM_SAMPLES)*100) ) # update displayed info, iter_percent = percentage of completion of current iteration (i,e epoch)
@@ -86,7 +86,7 @@ with tf.Session(config=config) as sess:
                 image = np.asarray(Image.fromarray(image, "RGB")) # necessary conversion for cv2
                 save_dir = os.path.join(LOG_PATH, "train_samples")
                 utils.save_p2d_image(image, p2d_gt_arr[0], p2d_pred_arr[0], save_dir, i+1)
-            
+
             elif (i+1) % LOG_ITER_FREQ == 0:
                 _, summary = sess.run([train_op, merged])
                 train_writer.add_summary(summary, i+1)
