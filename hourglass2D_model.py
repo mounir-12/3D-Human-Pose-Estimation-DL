8f0ef378ea74c1517ca72040b0fc51fa81f39dc6
ubuntu <mamrani@ethz.ch>
2019-05-23 03:27:52 +0200

    added namescopes

--- hourglass2D_model.py
+++ hourglass2D_model.py
@@ -20,73 +20,79 @@ class StackedHourglass:
         
     def __call__(self, inp, training=False):
         a = time.time()
-#        self.inp = tf.placeholder(self.dtype, shape=self.batch_shape) # the input batch of images
-#        self.training = tf.placeholder(tf.bool) # boolean used in batch_normalization layer, is True when training and False when predicting
         
         self.inp = inp
-        conv1 = conv_layer(inp, 64, (7, 7), (2, 2), (3, 3)) # image size goes from 256*256 to 128*128, 64 channels
-        relu1 = relu_layer(batch_norm_layer(conv1, training)) # apply batch norm then relu
-        res1 = residual_hg(relu1, 128, training) # produce 128 channels from 64 channels
-        max_pool1 = max_pool_layer(res1, (2, 2), (2, 2)) # 2x2 pooling with stride 2x2 => Down-sampling: we half the image height and width
-        
-        res2 = residual_hg(max_pool1, 128, training)
-        res3 = residual_hg(res2, self.nb_channels, training)
+        with tf.name_scope("preprocessing_input"):
+            conv1 = conv_layer(inp, 64, (7, 7), (2, 2), (3, 3)) # image size goes from 256*256 to 128*128, 64 channels
+            relu1 = relu_layer(batch_norm_layer(conv1, training)) # apply batch norm then relu
+            res1 = residual_hg(relu1, 128, training) # produce 128 channels from 64 channels
+            max_pool1 = max_pool_layer(res1, (2, 2), (2, 2)) # 2x2 pooling with stride 2x2 => Down-sampling: we half the image height and width
+            
+            res2 = residual_hg(max_pool1, 128, training)
+            res3 = residual_hg(res2, self.nb_channels, training)
         
         inter = res3 # intermediate input of the intermediate hourglass
         outputs = [] # the outputs of the hourglasses
         
         for i in range(self.nb_stacks):
-            hg_out = self.hourglass(inter, self.n, self.nb_channels, training) # push intermediate input through hourglass
-            hg_out = residual_block_hg(hg_out, self.nb_channels, self.nb_modules, training) # apply residual block one last time on gh_out which is the result of the last addition in 
-                                                                                            # the hourglass (see paper)
-            round1 = linear_layer(hg_out, self.nb_channels, training) # perform the 1st round of the 2 consecutive conv rounds, perform in addition to that a batch norm and relu
+            with tf.name_scope("hourglass_{}".format(i)):
+                hg_out = self.hourglass(inter, self.n, self.nb_channels, training) # push intermediate input through hourglass
+                hg_out = residual_block_hg(hg_out, self.nb_channels, self.nb_modules, training) # apply residual block one last time on gh_out which is the result of the last addition in 
+                                                                                                # the hourglass (see paper)
+            with tf.name_scope("hourglass_{}_lin_layer_1x1_conv".format(i)):
+                round1 = linear_layer(hg_out, self.nb_channels, training) # perform the 1st round of the 2 consecutive conv rounds, perform in addition to that a batch norm and relu
             
-            heatmaps = conv_layer(round1, self.nb_joins, (1, 1), (1, 1), (0, 0)) # perform a conv with 1x1 kernel, 1x1 stride and 0x0 padding to get the predicted heatmaps
+            with tf.name_scope("hourglass_{}_heatmaps".format(i)):
+                heatmaps = conv_layer(round1, self.nb_joins, (1, 1), (1, 1), (0, 0)) # perform a conv with 1x1 kernel, 1x1 stride and 0x0 padding to get the predicted heatmaps
             outputs.append(heatmaps) # store the predicted heatmaps
             
             if(i < self.nb_stacks-1): # if this isn't the last hourglass
-                round2 = conv_layer(round1, self.nb_channels, (1, 1), (1, 1), (0, 0)) # perform the 2nd round of the 2 consecutive conv rounds
-                transformed = conv_layer(heatmaps, self.nb_channels, (1, 1), (1, 1), (0, 0)) # transform the predictions back with a conv layer to prepare for add-op (by having compatible nb channels)
-                inter = inter + round2 + transformed # sum-up tensors which gives us the input for the next hourglass
+                with tf.name_scope("hourglass_{}_1x1_conv".format(i)):
+                    round2 = conv_layer(round1, self.nb_channels, (1, 1), (1, 1), (0, 0)) # perform the 2nd round of the 2 consecutive conv rounds
+                with tf.name_scope("hourglass_{}_transformed_heatmaps".format(i)):
+                    transformed = conv_layer(heatmaps, self.nb_channels, (1, 1), (1, 1), (0, 0)) # transform the predictions back with a conv layer to prepare for add-op
+                with tf.name_scope("hourglass_{}_input".format(i+1)):
+                    inter = inter + round2 + transformed # sum-up tensors which gives us the input for the next hourglass
         
         self.outputs = outputs
         
         final_output = outputs[-1]
         self.final_output = final_output
         
-        # since output_shape != input_shape (in paper, 256*256 input, 64*64 output), we compute the scaling factor along H and W
-        self.scale_H = tf.cast(tf.shape(final_output)[2] / tf.shape(inp)[2], tf.float32) # scale along H
-        self.scale_W = tf.cast(tf.shape(final_output)[3] / tf.shape(inp)[3], tf.float32) # scale along W
-        
-        batch_size = tf.shape(inp)[0]
-        self.scale = tf.tile(tf.reshape(tf.stack([self.scale_W, self.scale_H]), [1,1,2]), [batch_size, self.nb_joins, 1]) # tensor to rescale 2d poses to match output resolution 
-                                                                                                                          # (i,e the scaling from input to output)
-        # transform each set of heatmaps in the batch to a p2d
-        poses = []
-        for all_joints_heatmaps in tf.unstack(final_output): # for each set of 17 heatmaps (1 per joint) of the batch
-            joints = []
-            for joint_heatmap in tf.unstack(all_joints_heatmaps): # for each joint heatmap
-                joint = tf.reverse(tf.cast(tf.unravel_index(
-                                            tf.argmax(tf.reshape(joint_heatmap, [-1]), output_type=tf.int32), tf.shape(joint_heatmap)),tf.float32),
-                                    axis=[0]) # for each heatmap array "a", flatten array then get argmax index in flattened array then convert flat_index back to 2d index using 
-                                            # unravel_index then cast 2d index to float32, then swap the result (using tf.reverse) since the result is (row, col), but we want (col, row)=(x, y)
-                joints.append(joint)
+        with tf.name_scope("scaling"):
+            # since output_shape != input_shape (in paper, 256*256 input, 64*64 output), we compute the scaling factor along H and W
+            self.scale_H = tf.cast(tf.shape(final_output)[2] / tf.shape(inp)[2], tf.float32) # scale along H
+            self.scale_W = tf.cast(tf.shape(final_output)[3] / tf.shape(inp)[3], tf.float32) # scale along W
             
-            poses.append(tf.stack(joints))
+            batch_size = tf.shape(inp)[0]
+            self.scale = tf.tile(tf.reshape(tf.stack([self.scale_W, self.scale_H]), [1,1,2]), [batch_size, self.nb_joins, 1]) # tensor to rescale 2d poses to match output resolution 
+                                                                                                                              # (i,e the scaling from input to output)
+        with tf.name_scope("heatmaps_to_poses"):
+            # transform each set of heatmaps in the batch to a p2d
+            poses = []
+            for all_joints_heatmaps in tf.unstack(final_output): # for each set of 17 heatmaps (1 per joint) of the batch
+                joints = []
+                for joint_heatmap in tf.unstack(all_joints_heatmaps): # for each joint heatmap
+                    joint = tf.reverse(tf.cast(tf.unravel_index(
+                                                tf.argmax(tf.reshape(joint_heatmap, [-1]), output_type=tf.int32), tf.shape(joint_heatmap)),tf.float32),
+                                        axis=[0]) # for each heatmap array "a", flatten array then get argmax index in flattened array then convert flat_index back to 2d index using 
+                                                # unravel_index then cast 2d index to float32, then swap the result (using tf.reverse) since the result is (row, col), but we want (col, row)=(x, y)
+                    joints.append(joint)
+                
+                poses.append(tf.stack(joints))
+            
+            
+            p2d_pred_scaled_down = tf.stack(poses) # the heatmaps are scaled down since computed from the scaled_down output of resolution 64x64 (as opposed to input which is 256x256) 
+            
+            p2d_pred = p2d_pred_scaled_down / self.scale # divide by scale to scale up joints positions to input resolution
         
-        
-        p2d_pred_scaled_down = tf.stack(poses) # the heatmaps are scaled down since computed from the scaled_down output of resolution 64x64 (as opposed to input which is 256x256) 
-        
-        p2d_pred = p2d_pred_scaled_down / self.scale # divide by scale to scale up joints positions to input resolution
         self.p2d_pred = p2d_pred
-
         
         print("\n\nBuilt the model in {} s\n\n".format(time.time()-a))
 
         return outputs, p2d_pred # return all heatmaps of all hourglasses and the p2d predicted from the final_output
-            
-        
-        
+
+
     def hourglass(self, inp, n, nb_channels, training): # recursive definition of an hourglass, n=number of residual_blocks (cubes in paper) till the center of the hourglass
         up_branch = residual_block_hg(inp, nb_channels, self.nb_modules, training) # push input through residual block and get output
         
@@ -106,24 +112,26 @@ class StackedHourglass:
 
     def compute_loss(self, p2d_gt, all_heatmaps_pred):
         # we need to scale the ground-truth joints positions to account for the scaling from input to output image
-        p2d_gt_scaled = p2d_gt*self.scale # multiply each joint by the scale
-        
-        all_heatmaps = []
-        for p2d in tf.unstack(p2d_gt_scaled): # for each pose in the batch
-            heatmaps = []
-            for joint in tf.unstack(p2d): # for each joint in the pose
-                heatmap = self.gaussian_2d(joint, self.var, tf.shape(self.final_output)[2], tf.shape(self.final_output)[3]) # generate the 2d gaussian with same size as final_output
-                heatmaps.append(heatmap)
-                
-            all_heatmaps.append(tf.stack(heatmaps))
+        with tf.name_scope("gt_poses_to_heatmaps"):
+            p2d_gt_scaled = p2d_gt*self.scale # multiply each joint by the scale
             
-        heatmaps_gt = tf.stack(all_heatmaps) 
+            all_heatmaps = []
+            for p2d in tf.unstack(p2d_gt_scaled): # for each pose in the batch
+                heatmaps = []
+                for joint in tf.unstack(p2d): # for each joint in the pose
+                    heatmap = self.gaussian_2d(joint, self.var, tf.shape(self.final_output)[2], tf.shape(self.final_output)[3]) # generate the 2d gaussian with same size as final_output
+                    heatmaps.append(heatmap)
+                    
+                all_heatmaps.append(tf.stack(heatmaps))
+                
+            heatmaps_gt = tf.stack(all_heatmaps) 
         
-        total_loss = 0
-        for heatmaps_pred in all_heatmaps_pred:
-            loss = tf.losses.mean_squared_error(heatmaps_gt, heatmaps_pred)
-            total_loss += loss
-        self.loss = total_loss
+        with tf.name_scope("loss"):
+            total_loss = 0
+            for heatmaps_pred in all_heatmaps_pred:
+                loss = tf.losses.mean_squared_error(heatmaps_gt, heatmaps_pred)
+                total_loss += loss
+            self.loss = total_loss
 
 # Test code: uncomment to test
 #        with tf.Session() as sess:
@@ -146,22 +154,24 @@ class StackedHourglass:
                 
         return total_loss
         
-    def gaussian_2d(self, mean, var, H, W): # multivariate gaussian with diagonal covariance matrix Sigma = var * I 
-        mu = tf.cast(tf.cast(mean, tf.int32), tf.float32) # remove decimal part due to discretization of the 2D space
-        
-        # Generate a mesh grid to plot the distributions
-        X, Y = tf.meshgrid(tf.range(0.0, W, 1), tf.range(0.0, H, 1))
-        idx = tf.concat([tf.reshape(X, [-1, 1]), tf.reshape(Y,[-1,1])], axis=1)
-        # compute the gaussian over this mesh grid
-        prob = tf.reshape(tf.exp(-tf.reduce_sum(tf.square(idx-mean), axis=1)/(2*var)), tf.shape(X)) # compute unormalized 2D gaussian (so max value is 1) manually and reshape to [H, W]
-        return prob
+    def gaussian_2d(self, mean, var, H, W): # multivariate gaussian with diagonal covariance matrix Sigma = var * I
+        with tf.name_scope("gaussian"):
+            mu = tf.cast(tf.cast(mean, tf.int32), tf.float32) # remove decimal part due to discretization of the 2D space
+            
+            # Generate a mesh grid to plot the distributions
+            X, Y = tf.meshgrid(tf.range(0.0, W, 1), tf.range(0.0, H, 1))
+            idx = tf.concat([tf.reshape(X, [-1, 1]), tf.reshape(Y,[-1,1])], axis=1)
+            # compute the gaussian over this mesh grid
+            prob = tf.reshape(tf.exp(-tf.reduce_sum(tf.square(idx-mean), axis=1)/(2*var)), tf.shape(X)) # compute unormalized 2D gaussian (so max value is 1) manually and reshape to [H, W]
+            return prob
             
     def get_train_op(self, loss, learning_rate=0.001):
-        self.global_step = tf.Variable(0, name='global_step',trainable=False)
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS) # for batch norm
-        with tf.control_dependencies(update_ops):
-            self.train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, self.global_step)
-        return self.train_op, self.global_step
+        with tf.name_scope("train"):
+            self.global_step = tf.Variable(0, name='global_step',trainable=False)
+            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS) # for batch norm
+            with tf.control_dependencies(update_ops):
+                self.train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, self.global_step)
+            return self.train_op, self.global_step
         
 
         
--- layers.py
+++ layers.py
@@ -4,86 +4,96 @@ from tensorflow.contrib.layers import xavier_initializer
 """ all functions below expect input in channels first format for perfomance on GPU """
 
 def padding_layer(inp, padding):
-    pad_H = padding[0]
-    pad_W = padding[1]
-    if pad_H > 0 or pad_W > 0: # perform padding only if padding by 1 pixel or more
-        paddings = [[0, 0], [0, 0], [pad_H, pad_H], [pad_W, pad_W]] # pad only along the height and width dimension, no padding along the batch and channels dimensions
-        return tf.pad(inp, paddings)
-    else:
-        return inp
+    with tf.name_scope("padding_layer"):
+        pad_H = padding[0]
+        pad_W = padding[1]
+        if pad_H > 0 or pad_W > 0: # perform padding only if padding by 1 pixel or more
+            paddings = [[0, 0], [0, 0], [pad_H, pad_H], [pad_W, pad_W]] # pad only along the height and width dimension, no padding along the batch and channels dimensions
+            return tf.pad(inp, paddings)
+        else:
+            return inp
 
 def conv_layer(inp, out_channels, filter_size=1, strides=1, padding="same", use_bias=False): 
-    
-    if isinstance(padding, (list, tuple)): # if a list/tuple passed
-        inp = padding_layer(inp, padding)
-        padding = "valid"
+    with tf.name_scope("conv_layer"):
+        if isinstance(padding, (list, tuple)): # if a list/tuple passed
+            inp = padding_layer(inp, padding)
+            padding = "valid"
 
-    return tf.layers.conv2d(inputs=inp, filters=out_channels, kernel_size=filter_size, strides=strides, padding=padding, 
-                            data_format="channels_first", use_bias=use_bias, kernel_initializer=xavier_initializer(), 
-                            bias_initializer=xavier_initializer())
+        return tf.layers.conv2d(inputs=inp, filters=out_channels, kernel_size=filter_size, strides=strides, padding=padding, 
+                                data_format="channels_first", use_bias=use_bias, kernel_initializer=xavier_initializer(), 
+                                bias_initializer=xavier_initializer())
 
 def relu_layer(inp):
-    return tf.nn.relu(inp)
+    with tf.name_scope("relu_layer"):
+        return tf.nn.relu(inp)
 
 def batch_norm_layer(inp, training, momentum=0.99, epsilon=0.001):
-    return tf.layers.batch_normalization(inputs=inp, axis=1, momentum=momentum, epsilon=epsilon, training=training) # axis=1 because channels first
+    with tf.name_scope("batch_norm_layer"):
+        return tf.layers.batch_normalization(inputs=inp, axis=1, momentum=momentum, epsilon=epsilon, training=training) # axis=1 because channels first
     
 def max_pool_layer(inp, pool_size=1, strides=1, padding="valid"):
-    if isinstance(padding, (list, tuple)): # if a list/tuple passed
-        inp = padding_layer(inp, padding)
-        padding = "valid"
-        
-    return tf.layers.max_pooling2d(inp, pool_size=pool_size, strides=strides, padding=padding, data_format="channels_first")
+    with tf.name_scope("max_pool_layer"):
+        if isinstance(padding, (list, tuple)): # if a list/tuple passed
+            inp = padding_layer(inp, padding)
+            padding = "valid"
+            
+        return tf.layers.max_pooling2d(inp, pool_size=pool_size, strides=strides, padding=padding, data_format="channels_first")
     
 def nearest_neighbor_up_sampling_layer(inp, factor):
-#    return tf.keras.layers.UpSampling2D(size=factor, data_format="channels_first", interpolation='nearest').apply(inp)
-    inp = tf.transpose(inp, [0, 2, 3, 1]) # convert to channels_last format
-    
-    new_H = tf.shape(inp)[1] * factor[0] # the new heigh
-    new_W = tf.shape(inp)[2] * factor[1] # the new width
-    out =  tf.image.resize_nearest_neighbor(inp, (new_H, new_W)) # resize images with nearest neighbor up sampling
+    with tf.name_scope("nearest_neighbor_up_sampling_layer"):
+#        return tf.keras.layers.UpSampling2D(size=factor, data_format="channels_first", interpolation='nearest').apply(inp)
+        inp = tf.transpose(inp, [0, 2, 3, 1]) # convert to channels_last format
+        
+        new_H = tf.shape(inp)[1] * factor[0] # the new heigh
+        new_W = tf.shape(inp)[2] * factor[1] # the new width
+        out =  tf.image.resize_nearest_neighbor(inp, (new_H, new_W)) # resize images with nearest neighbor up sampling
 
-    return tf.transpose(out, [0, 3, 1, 2]) # convert to channels_first format and return result
+        return tf.transpose(out, [0, 3, 1, 2]) # convert to channels_first format and return result
     
 # --------------------------------------- Stacked Hourglass Model functions --------------------------------------------------------
     
 def conv_block_hg(inp, out_channels, training): # Main convolutional block of hourglass model
-    tmp = batch_norm_layer(inp, training)
-    tmp = relu_layer(tmp)
-    tmp = conv_layer(tmp, out_channels/2, (1, 1)) # 1x1 kernel, 1x1 stride so the input width and height are conserved and "out_channels/2" channels are produced
-    
-    tmp = batch_norm_layer(tmp, training)
-    tmp = relu_layer(tmp)
-    tmp = conv_layer(tmp, out_channels/2, (3, 3), (1, 1), (1, 1)) # 3x3 kernel 1x1 stride, 1x1 padding, so the input width and height are conserved and "out_channels/2" channels are produced
-    
-    
-    tmp = batch_norm_layer(tmp, training)
-    tmp = relu_layer(tmp)
-    out = conv_layer(tmp, out_channels, (1, 1)) # 1x1 kernel, 1x1 stride so the input width and height are conserved and "out_channels" channels are produced
-    
-    return out
+    with tf.name_scope("conv_block_hg"):
+        tmp = batch_norm_layer(inp, training)
+        tmp = relu_layer(tmp)
+        tmp = conv_layer(tmp, out_channels/2, (1, 1)) # 1x1 kernel, 1x1 stride so the input width and height are conserved and "out_channels/2" channels are produced
+        
+        tmp = batch_norm_layer(tmp, training)
+        tmp = relu_layer(tmp)
+        tmp = conv_layer(tmp, out_channels/2, (3, 3), (1, 1), (1, 1)) # 3x3 kernel 1x1 stride, 1x1 padding, so the input width and height are conserved and "out_channels/2" channels are produced
+        
+        
+        tmp = batch_norm_layer(tmp, training)
+        tmp = relu_layer(tmp)
+        out = conv_layer(tmp, out_channels, (1, 1)) # 1x1 kernel, 1x1 stride so the input width and height are conserved and "out_channels" channels are produced
+        
+        return out
 
 def skip_layer_hg(inp, out_channels): # Skip Layer of hourglass
-    in_channels = inp.get_shape()[1].value # get nb of channels of input
-    if in_channels == out_channels: # if same number of channels
-        return tf.identity(inp) # just return a copy of the input
-    else: # otherwise
-        return conv_layer(inp, out_channels, (1, 1)) # 1x1 kernel, 1x1 stride, "out_channels" channels are produced
+    with tf.name_scope("skip_layer_hg"):
+        in_channels = inp.get_shape()[1].value # get nb of channels of input
+        if in_channels == out_channels: # if same number of channels
+            return inp # just the input
+        else: # otherwise
+            return conv_layer(inp, out_channels, (1, 1)) # 1x1 kernel, 1x1 stride, "out_channels" channels are produced
             
 def residual_hg(inp, out_channels, training): # residual module of hourglass
-    out_1 = conv_block_hg(inp, out_channels, training) # push the input through the conv block
-    out_2 = skip_layer_hg(inp, out_channels) # push the input through the skip layer
-    return out_1 + out_2 # sum up and return result
+    with tf.name_scope("residual_module_hg"):
+        out_1 = conv_block_hg(inp, out_channels, training) # push the input through the conv block
+        out_2 = skip_layer_hg(inp, out_channels) # push the input through the skip layer
+        return out_1 + out_2 # sum up and return result
 
 def residual_block_hg(inp, out_channels, nb_times, training): # residual block consisting of residual_modules stacked nb_times
-    out = inp
-    for _ in range(max(1, nb_times)): # apply residual module at least 1 time
-        out = residual_hg(out, out_channels, training)
-    return out
+    with tf.name_scope("residual_block_hg"):
+        out = inp
+        for _ in range(max(1, nb_times)): # apply residual module at least 1 time
+            out = residual_hg(out, out_channels, training)
+        return out
         
 def linear_layer(inp, out_channels, training):
-    inp = conv_layer(inp, out_channels, (1, 1), (1, 1), (0, 0)) # 1x1 kernel, 1x1 stride, 0x0 padding so input width and height are conserved and "out_channels" channels are produced
-    return relu_layer(batch_norm_layer(inp, training)) # perform batch norm then relu
+    with tf.name_scope("linear_layer"):
+        inp = conv_layer(inp, out_channels, (1, 1), (1, 1), (0, 0)) # 1x1 kernel, 1x1 stride, 0x0 padding so input width and height are conserved and "out_channels" channels are produced
+        return relu_layer(batch_norm_layer(inp, training)) # perform batch norm then relu
 
 
 
